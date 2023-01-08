0af85379b925025cf2760189c6e11a882db20745
Muhammad Aqeel Afzal <i190650@nu.edu.pk>
2023-01-08 23:33:57 +0500

    Delete backend directory

--- backend/testsuite/java/AllTests.java
+++
@@ -1,9 +0,0 @@
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ ManageUser.class,ManageClasses.class })
-public class AllTests {
-
-}
